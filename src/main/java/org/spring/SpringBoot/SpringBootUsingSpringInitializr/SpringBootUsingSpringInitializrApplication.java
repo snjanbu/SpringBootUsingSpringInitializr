5bc0b1cda91977a60f566862036854790afb251c
Sanjayanbu <snjanbu@gmail.com>
2018-08-26 16:16:51 +0530

    refs #ticket-learning SpringBoot Implementation

---
+++ src/main/java/org/spring/SpringBoot/SpringBootUsingSpringInitializr/SpringBootUsingSpringInitializrApplication.java
@@ -0,0 +1,12 @@
+package org.spring.SpringBoot.SpringBootUsingSpringInitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsingSpringInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsingSpringInitializrApplication.class, args);
+	}
+}
